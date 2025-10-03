25bfa4fdc28f83754830a1ba571c0b23ba386fba
Senryoku <maretverdant@gmail.com>
2025-10-03 05:23:16 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -112,6 +112,7 @@ pub fn build(b: *std.Build) void {
 
     const zdawn = b.addLibrary(.{
         .name = "zdawn",
+        .linkage = .dynamic,
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
